1b33cf22df67e1afe1f900668a832a2365c6683a
Cain Brouwer <caindavid2008@gmail.com>
2026-03-29 20:11:52 +0200

    Begin van opdracht 2

--- Opdracht 2/FE-S2-P3-Opdracht2-master/script.js
+++ Opdracht 2/FE-S2-P3-Opdracht2-master/script.js
@@ -1,6 +1,20 @@
 // Maak een variabele aan genaamd: subtractButtons en selecteer alle buttons met de klasse subtract.
 // Maak een variabele aan genaamd: addButtons en selecteer alle buttons met de klasse add.
 
-// Maak een variabele aan genaamd: counterEl en selecteer het div element met het id counter.
+// Maak een variabele aan genaamd: counterEl en selecteer het div. element met het id counter.
 // Maak een variabele aan genaamd: counterVal en selecteer het innerHTML attribuut van de counterEl variabele.
 // - - Omdat counterVal een variabele is die moet veranderen moeten we "let" gebruiken in plaats van "const"
+
+
+const counterEl = document.getElementById("counter");
+
+const innerHTML = counterEl.textContent;
+
+let counterVal = parseInt(innerHTML, 10);
+
+const add1 =  document.getElementsByClassName('add1');
+    addEventListener('click', () => {})
+
+const add10 = document.getElementsByClassName('add10');
+    add10.addEventListener('click', () => {})
+
