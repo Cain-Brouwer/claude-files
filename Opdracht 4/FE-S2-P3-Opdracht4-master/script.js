8c627975bd8199f0a93a14d4b90ec78de68a3a9f
Cain Brouwer <caindavid2008@gmail.com>
2026-04-15 19:09:57 +0200

    Opdracht 4 gemaakt voor Leerjaar 1 - Periode 3 - Frontend

--- Opdracht 4/FE-S2-P3-Opdracht4-master/script.js
+++ Opdracht 4/FE-S2-P3-Opdracht4-master/script.js
@@ -1,14 +1,31 @@
 const modalElement = document.querySelector('div#modal');
+const openButton = document.querySelector('#open-modal');
+const closeButton = document.querySelector('#close-modal');
 
-const openButton = document.getElementsByTagName('button#open-modal');
+const closeModal = () => {
+  modalElement.classList.remove('visible');
+  setTimeout(() => {
+    modalElement.style.display = 'none';
+  }, 1200);
+};
 
 openButton.addEventListener('click', () => {
   modalElement.style.display = 'flex';
-  setTimeout(() => { modalElement.classList.add('visible') }, 5);
+  setTimeout(() => {
+    modalElement.classList.add('visible');
+  }, 5);
 });
 
-// @TODO: create variable closeButton and select div with id close-modal
+closeButton.addEventListener('click', closeModal);
 
-// @TODO: add an EventListener to closeButton variable.
-// @TODO: first class visible has to be removed.
-// @TODO: after 1200 miliseconds style.display should be none.
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+
+modalElement.addEventListener('click', (event) => {
+  if (event.target === modalElement) {
+    closeModal();
+  }
+});
